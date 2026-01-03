3ec97644cf1fedcdf2ea83282cd5952ec546e246
Patson T Lawrence <patsont90@gmail.com>
2026-01-03 16:12:04 +0300

    update

--- firebase.ts
+++ firebase.ts
@@ -2,14 +2,16 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDeIN9fdcmTCbMSq-1y2BsVHO1BokbETgc",
-  authDomain: "stockwellbsl.firebaseapp.com",
-  projectId: "stockwellbsl",
-  storageBucket: "stockwellbsl.firebasestorage.app",
-  messagingSenderId: "154274776156",
-  appId: "1:154274776156:web:00219df37ebaf86e9c9299",
+
+  const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
 };
+
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
