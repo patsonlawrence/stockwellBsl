0605bc10bbd33ed51c155b108a6068c168a3a9e0
Patson T Lawrence <patsont90@gmail.com>
2026-01-05 20:01:07 +0300

    update

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -3,6 +3,10 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(req: NextRequest) {
   const { email, password } = await req.json();
 
+  console.log('FIREBASE_PRIVATE_KEY length:', process.env.FIREBASE_PRIVATE_KEY?.length);
+  console.log('FIREBASE_PROJECT_ID:', process.env.FIREBASE_PROJECT_ID);
+
+
   if (email === "test@example.com" && password === "password123") {
     return NextResponse.json({ success: true, message: "Login successful!" });
   }
