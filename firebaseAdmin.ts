b94fe18e16d418f1f29c2854dc1aae5394f0842d
Patson T Lawrence <patsont90@gmail.com>
2026-02-01 15:21:50 +0300

    update

--- firebaseAdmin.ts
+++ firebaseAdmin.ts
@@ -3,6 +3,12 @@ import admin from "firebase-admin";
 
 function getApp() {
   if (admin.apps.length) return admin.apps[0];
+  console.log("ENV CHECK", {
+  FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+  FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL,
+  FIREBASE_PRIVATE_KEY_BASE64: !!process.env.FIREBASE_PRIVATE_KEY_BASE64,
+});
+
 
   if (
     !process.env.FIREBASE_PROJECT_ID ||
